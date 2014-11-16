c09b8a39cc6509c4091d0441c70de87668be8ed9
seza443 <github.tanguy.vaessen@hotmail.com>
2014-11-16 22:39:26 +0100

    structure m5

---
+++ Code/src/Controller/Main.java
@@ -0,0 +1,15 @@
+package Controller;
+
+/**
+ * Le point d'entrée du programme est ici 
+ * @author Tanguy
+ *
+ */
+public class Main {
+	/**
+     * @param args les arguments de la ligne de commande
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello m5");
+    }
+}
