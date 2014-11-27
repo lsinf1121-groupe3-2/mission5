93c2b78414f4b831b206e4d515475b4bea2cd014
seza443 <github.tanguy.vaessen@hotmail.com>
2014-11-27 16:36:04 +0100

    Compress and Decompress programs

--- Code/src/compress/controller/Compress.java
+++ Code/src/compress/controller/Compress.java
@@ -3,5 +3,6 @@ package compress.controller;
 public class Compress {
 	public static void main(String[] args){
 		//TODO
+		System.out.println("Compress program");
 	}
 }
--- Code/src/decompress/controller/Decompress.java
+++ Code/src/decompress/controller/Decompress.java
@@ -3,5 +3,6 @@ package decompress.controller;
 public class Decompress {
 	public static void main(String[] args){
 		//TODO
+		System.out.println("Decompress program");
 	}
 }
