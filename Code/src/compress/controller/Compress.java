6f63a290f8280918cea9264020f9fd2bc3a376f5
seza443 <github.tanguy.vaessen@hotmail.com>
2014-11-27 23:53:01 +0100

    Stub Decompress

--- Code/src/compress/controller/Compress.java
+++ Code/src/compress/controller/Compress.java
@@ -42,7 +42,7 @@ public class Compress {
 	}
 
 	/**
-	 * Controleur par défaut
+	 * Constructeur par défaut
 	 */
 	public Compress(){
 		charFrequency = new HashMap<>();
--- Code/src/decompress/controller/Decompress.java
+++ Code/src/decompress/controller/Decompress.java
@@ -1,14 +1,23 @@
 package decompress.controller;
 
+import java.io.IOException;
+
+import business.HuffmanBTree;
+import utils.*;
+
 /**
  * Use: java Decompress inputCompressedFile [outputFile]
  * @author Tanguy, Alexandre, mathieu, Jonathan
  *
  */
 public class Decompress {
-	String commandFile;
+	String inputFile;
     String outputFile;
     String defaultFile = "resultFile.txt";
+    int nbrCharToRead = 0;
+	private HuffmanBTree huffmanBTree;
+	OutputBitStream out;
+	InputBitStream in;
 	
 	/**
 	 * Point d'entrée du programme Decompress
@@ -20,7 +29,7 @@ public class Decompress {
 	}
 
 	/**
-	 * Controleur par défaut
+	 * Constructeur par défaut
 	 */
 	public Decompress(){
 		
@@ -32,7 +41,11 @@ public class Decompress {
 	 */
 	private void start(String[] args) {
 		this.parseArgs(args);
+		//TODO: ouvrir et initiliaser les flux d'entrées et de sorties (InputBitStream et OutputBitStream)
+		this.nbrCharToRead = this.readNbrCharCompressed();
+		this.huffmanBTree = this.readHuffmanBTree();
 		this.decompressFile();
+		//TODO: fermer les flux
 	}
 	
 	/**
@@ -41,7 +54,7 @@ public class Decompress {
      */
     private void parseArgs(String[] args){
 		if (args.length > 0 && args.length <= 2 && args[0] != null && !args[0].isEmpty()) { 
-    		this.commandFile = args[0];
+    		this.inputFile = args[0];
     		
     		if(args.length > 1 && args[1] != null && !args[1].isEmpty() ) {
     			this.outputFile = args[1];
@@ -54,8 +67,47 @@ public class Decompress {
 			System.exit(-1);
 		}
     }
+
+	/**
+	 * @pre inputFile pointe vers un fichier compressé
+	 * @post lit la première information contenue dans le fichier compressé, à savoir le nombre de caractères qui ont été compressés et écrit dedans.
+	 * Retourne cette valeur.
+	 */
+	private int readNbrCharCompressed() {
+		// TODO 
+		// Utiliser InputBitStream.readInt()
+		return 0;
+	}
 	
-	public void decompressFile(){
-		
+	/**
+	 * @pre inputFile pointe vers un fichier compressé
+	 * @post Lit et interprete la deuxième information contenue dans le fichier compressé, à savoir l'arbre de Huffman.
+	 * 		Cet arbre est ecnodé en binaire.
+	 * 		Un bit de valeur 0 signifie qu'on doit ajouter un noeud interne à l'arbre + 2 noeuds enfants et qu'on se déplace vers le fils de gauche
+	 * 		Un but à 1 signifie qu'on lit une feuille et sera suivi de la représentation (CHAR) de cette feuille
+	 * 		On est capable de déduire quand on a lu l'arbre en entier à partir du moment où l'on a lu le dernier fils droit du dernier noeud interne.
+	 * 		
+	 */
+	private HuffmanBTree readHuffmanBTree() {
+		// TODO read bit by bit
+		//if 1 then read Char
+		return null;
+	}
+
+    /**
+     * @pre inputFile pointe vers un fichier compressé
+     * 		outpuFile est un chemin valide vers le fichier de sortie qui contiendra la version décompressée.
+     * 		nbrCharToRead indique le nombre de caractère qui devront être lus
+     * 		huffmanBTree est un arbre Huffman permettant de décoder correctement le fichier
+     * @post Le reste du fichier inputFile est lu bit par bit.
+     * 		Pour chaque bit, on parcourt l'arbre de Huffman.
+     * 			Si on lit un bit à 0, on descend vers le fils gauche.
+     * 			Si on lit un bit à 1, on descend vers le fils droit.
+     * 			On s'arrete lorsqu'on arrive à une feuille.
+     * 			On écrit le caractère correspondant dans le fichier outputFile.
+     * 		On répète l'opération jusqu'à avoir lu nbrCharToRead caractères.
+     */
+	private void decompressFile(){
+
 	}
 }
